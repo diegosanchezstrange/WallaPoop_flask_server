9775286017019b1be40ebc1529c44d0827423f3b
diegosanchezstrange <diegosanchezstrangejavax@gmail.com>
2020-02-24 13:48:10 +0100

    Basic Flask config added

---
+++ src/server/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+
+app_settings = os.getenv('APP_SETTINGS', 'src.server.config.DevelopmentConfig')
+
+app.config.from_object(app_settings)
+
+#Just for development on localhost
+app.config['MYSQL_USER']='diego'
+app.config['MYSQL_PASSWORD']='p@ssword'
+app.config['MYSQL_DB']='wallapoop'
+app.config['MYSQL_HOST']='localhost'
+
+#Creating the database instance
+db = MySQL(app)
